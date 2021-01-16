ece19f21b9d18e30c158bf9ed00d933a2fb5d2f8
“jyoti.lata” <jyoti.lata@payu.in>
2021-01-16 21:36:34 +0530

    first commit

---
+++ src/main/java/com/simplilearn/docker_jenkins_integration/App.java
@@ -0,0 +1,13 @@
+package com.simplilearn.docker_jenkins_integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
