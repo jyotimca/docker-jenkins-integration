e7f26602159247c1923d2916ea96c59b8e8930b6
“jyoti.lata” <jyoti.lata@payu.in>
2021-01-17 22:35:40 +0530

    second commit

---
+++ src/test/java/com/simplilearn/docker_jenkins_integration/testRun.java
@@ -0,0 +1,12 @@
+package com.simplilearn.docker_jenkins_integration;
+
+public class testRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("checking jenkins pipeline");
+
+	}
+
+}
